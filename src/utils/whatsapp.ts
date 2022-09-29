93063b901116c0cfbc6c6c3c04690c1aa4c7fa6b
Gichia <petergichia35@gmail.com>
2022-09-29 11:57:42 +0300

    create a whatsapp util class that will handle whatsapp functionalities and interact with the meta api

---
+++ src/utils/whatsapp.ts
@@ -0,0 +1,71 @@
+import axios from 'axios';
+
+export class Whatsapp {
+  bearerToken: string;
+  phoneNumberId: string;
+  displayPhoneNumber: string;
+
+  constructor({
+    bearerToken,
+    phoneNumberId,
+    displayPhoneNumber,
+  }: {
+    bearerToken: string;
+    phoneNumberId: string;
+    displayPhoneNumber: string;
+  }) {
+    this.bearerToken = bearerToken;
+    this.phoneNumberId = phoneNumberId;
+    this.displayPhoneNumber = displayPhoneNumber;
+  }
+
+  async markMessageRead(msgId: string): Promise<string> {
+    const fbUrl = `https://graph.facebook.com/v12.0/${this.phoneNumberId}/messages`;
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.bearerToken}`,
+      },
+    };
+
+    const payload = {
+      status: 'read',
+      message_id: msgId,
+      messaging_product: 'whatsapp',
+    };
+
+    const response = await axios.post<string>(fbUrl, payload, config);
+    const respData = response.data;
+
+    return respData;
+  }
+
+  async sendMessageResponse({
+    messageResponse,
+    phoneNumberFrom,
+  }: {
+    phoneNumberFrom: string;
+    messageResponse: string;
+  }): Promise<string> {
+    const fbUrl = `https://graph.facebook.com/v12.0/${this.phoneNumberId}/messages`;
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.bearerToken}`,
+      },
+    };
+
+    const payload = {
+      to: `${phoneNumberFrom}`,
+      messaging_product: 'whatsapp',
+      text: { body: `${messageResponse}` },
+    };
+
+    const response = await axios.post<string>(fbUrl, payload, config);
+    const respData = response.data;
+
+    return respData;
+  }
+}
