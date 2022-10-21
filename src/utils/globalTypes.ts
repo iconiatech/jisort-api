8bf957d0ef5cf9ffb3d18b4a0ca43672509ecd5e
Gichia <petergichia35@gmail.com>
2022-10-21 09:02:31 +0300

    create global types for menu response and action types

---
+++ src/utils/globalTypes.ts
@@ -0,0 +1,12 @@
+export enum MenuActionType {
+  TICKET = 'ticket',
+  ANSWER = 'answer',
+  SUBMENUS = 'subMenus',
+  PRODUCTS = 'products',
+}
+
+export enum MenuResponseType {
+  NUMBER = 'number',
+  STRING = 'string',
+  NONE = 'none',
+}
